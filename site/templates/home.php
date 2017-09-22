cd57db5971ef0a8e6c2d7b1e4a7187abc5e256e3
Hanahan <jonathan@milieu.works>
2017-09-21 22:07:12 -0700

    building seperate map page to cover for failure to load

--- site/templates/home.php
+++ site/templates/home.php
@@ -2,32 +2,32 @@
 <?php snippet('menu') ?>
 
 
-<body>
+
 <script type="text/javascript" src="assets/js/sundaymap.js"></script>
 <script async defer
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDieZ7uAY4DPdT3Z4fp4KtykHl6dWryYdw&callback=initMap">
-    </script>
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDieZ7uAY4DPdT3Z4fp4KtykHl6dWryYdw&callback=initMap">
+</script>
 
-
+<div class="container home">
   <div id="mapContainer">
     <div id="mapCanvas" ></div>
   </div>
 
   <div class="logo">
-  <img src="assets/images/Splash_Logo_BW.png"/>
+    <img src="assets/images/Splash_Logo_BW.png"/>
   </div>
 
   <div id="saturday">
-  <h1>Saturday</h1>
+    <h1>Saturday</h1>
   </div>
   <div id="sunday">
-  <h1>Sunday</h1>
-    </div>
+    <h1>Sunday</h1>
+  </div>
 
-<div id="date">
-  <h1>October 7–8, 2017</h1>
-    </div>
-	
-  </body>
+  <div id="date">
+    <h1>October 7–8, 2017</h1>
+  </div>
+</div>
+
 
 <?php snippet('footer') ?>
